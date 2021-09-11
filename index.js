7869b97720c8b53b56637a9355dda36c8a187799
Scott Isitt <isitts@hotmail.com>
2021-09-10 20:51:34 -0700

    Change routes to stay on same page

--- index.js
+++ index.js
@@ -28,8 +28,10 @@ app.get("/", (req, res) => {
     res.render("index")
 })
 
+// Request form for page 1
 app.get("/story", async (req, res) => {
     const story = await Story.findOne({}) // <-- finding first page of story...
+    // res.send(story)
     res.redirect(`/story/${story._id}`) // <-- storing the value and passing it
 })                                             // to /story page.
 
@@ -37,17 +39,27 @@ app.get("/story", async (req, res) => {
 // I don't want the id showing in the url, but this works when I copy and paste the
 // story id from the database. But I can't find it with the code below.*
 // * Fixed. I needed to submit a post request from index.ejs with the db id attached.
+
+// Story page 1 render
 app.get("/story/:id", async (req, res) => {
     const { id } = req.params
     const story = await Story.findById(id)
     res.render("story", { story })
 })
 
-app.post("/story/:id", async (req, res) => {
-    const { id } = req.params
-    const story = await Story.findById(id)
-    res.render(`story/${story._id}`, { story })
+// Story page 2 request form
+app.get("/story2", async (req, res) => {
+    const story2 = await Story.findOne({ _id: "6108dc5c8da8033b0f0b476c" }) // <-- finding first page of story...
+    // console.log(story2)
+    res.redirect(`/story/${story2._id}`) // <-- storing the value and passing it
 })
+
+// Story page 2 render
+// app.get("/story2/:id", async (req, res) => {
+//     const { id } = req.params
+//     const story2 = await Story.findById(id)
+//     res.render("story2", { story2 })
+// })
 
 
 

b121380ba92a7f8fafff57571b8e0d62c6b6c157
Scott Isitt <isitts@hotmail.com>
2021-09-10 23:14:22 -0700

    insertMany new story segments with nextPage keys.

--- seeds/storySeeds.js
+++ seeds/storySeeds.js
@@ -15,14 +15,42 @@ db.once("open", () => {
 })
 
 const seedDB = async () => {
-    // await Story.deleteMany({})
-    const story = new Story({
-        title: "Moving on...",
-        segment: "You keep walking, but as you do, you start to wonder about the shiny object more. For some reason, it doesn't leave your thoughts.",
-        option1: "Go back to pick it up.",
-        option2: "Shrug and keep walking."
-    })
-    await story.save()
+    await Story.deleteMany({})
+    await Story.insertMany([
+        {
+            title: "Where to go...",
+            segment: "You are walking down the street when you see a shiny object partway down the alley to your right. You look at it with curiosity. Do you go see what it is...or do you keep walking?",
+            option1: "Go see what it is.",
+            option2: "Keep walking...",
+            nextPageA: "p2a",
+            nextPageB: "p2b"
+        },
+        {
+            title: "The Shiny Object",
+            segment: "You walk up to the shiny object and see that it's a locket. It looks somewhat familiar, but you can't remember if you've seen it before or not.",
+            option1: "Open the locket.",
+            option2: "Put it back down.",
+            nextPageA: "p3a",
+            nextPageB: "p3b"
+        },
+        {
+            title: "Moving on...",
+            segment: "You keep walking, but as you do, you start to wonder about the shiny object more. For some reason, it doesn't leave your thoughts.",
+            option1: "Go back to pick it up.",
+            option2: "Shrug and keep walking.",
+            nextPageA: "p3c",
+            nextPageB: "p3d"
+        }
+    ])
+    // const story = new Story({
+    //     title: "",
+    //     segment: "",
+    //     option1: "",
+    //     option2: "",
+    //     nextPageA: "",
+    //     nextPageB: ""
+    // })
+    // await story.save()
 }
 
 seedDB().then(() => {

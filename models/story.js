bf5a76d6a80468e4bd33630de3c39b73f4e687c0
Scott Isitt <isitts@hotmail.com>
2021-10-15 23:42:51 -0700

    Format three options in schema and show page.

--- models/story.js
+++ models/story.js
@@ -4,11 +4,17 @@ const Schema = mongoose.Schema
 
 const storySchema = new Schema({
     title: String,
-    segment: String,
+    segment1: String,
+    segment2: String,
+    segment3: String,
+    segment4: String,
+    segment5: String,
     option1: String,
     option2: String,
+    option3: String,
     nextPageA: String,
     nextPageB: String,
+    nextPageC: String,
     notes: String
 })
 

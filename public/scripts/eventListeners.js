b79f6a897e315aec426f631cf2a028a1c8c5a8cb
Scott Isitt <isitts@hotmail.com>
2021-11-26 18:55:17 -0800

    Commment out eventListeners.js in styes directory.

--- public/scripts/eventListeners.js
+++ public/scripts/eventListeners.js
@@ -1,9 +1,9 @@
-let option1 = document.querySelector("#option1");
-option1.addEventListener("click", () => {
-    console.log("You clicked option 1.")
-});
+// let option1 = document.querySelector("#option1");
+// option1.addEventListener("click", () => {
+//     console.log("You clicked option 1.")
+// });
 
-let option2 = document.querySelector("#option2");
-option2.addEventListener("click", () => {
-    console.log("You clicked option 2.")
-});
+// let option2 = document.querySelector("#option2");
+// option2.addEventListener("click", () => {
+//     console.log("You clicked option 2.")
+// });
